e3ca8365ec28a72205c2365025f3976c9d3167d1
mztakeyocrackers-lang <mztakeyocrackers@gmail.com>
2026-04-23 00:03:38 -0500

    Add logout handler to clear session cookies

---
+++ api/discord/logout.js
@@ -0,0 +1,9 @@
+const { SESSION_COOKIE, STATE_COOKIE, sendJson, serializeCookie } = require('./_lib');
+
+module.exports = async function handler(req, res) {
+  res.setHeader('Set-Cookie', [
+    serializeCookie(SESSION_COOKIE, '', { maxAge: 0 }),
+    serializeCookie(STATE_COOKIE, '', { maxAge: 0 })
+  ]);
+  sendJson(res, 200, { ok: true });
+};
