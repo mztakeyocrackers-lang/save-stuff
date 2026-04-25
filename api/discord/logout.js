fcc4408d94cdf0d6f3f5ae4e359a59c60cf90264
mztakeyocrackers-lang <mztakeyocrackers@gmail.com>
2026-04-24 23:06:46 -0500

    Add RETURN_COOKIE to logout handler

--- api/discord/logout.js
+++ api/discord/logout.js
@@ -1,9 +1,10 @@
-const { SESSION_COOKIE, STATE_COOKIE, sendJson, serializeCookie } = require('./_lib');
+const { RETURN_COOKIE, SESSION_COOKIE, STATE_COOKIE, sendJson, serializeCookie } = require('./_lib');
 
 module.exports = async function handler(req, res) {
   res.setHeader('Set-Cookie', [
     serializeCookie(SESSION_COOKIE, '', { maxAge: 0 }),
-    serializeCookie(STATE_COOKIE, '', { maxAge: 0 })
+    serializeCookie(STATE_COOKIE, '', { maxAge: 0 }),
+    serializeCookie(RETURN_COOKIE, '', { maxAge: 0 })
   ]);
   sendJson(res, 200, { ok: true });
 };
